086d228d8668b2541d4bbd34d30f499f9ab72294
lokesh <19h41a0485@gmail.com>
2024-07-19 19:33:31 +0530

    init

---
+++ public/src/scripts/file.js
@@ -0,0 +1,27 @@
+function imageclick1(){
+    document.querySelector("p").innerHTML=`brand  ; jockey.<br>
+    product : tshirt,<br>
+    price: 1200,<br>
+    stock: available `
+}
+function imageclick2(){
+    document.querySelector("p").innerHTML=`brand  ; nike,<br>
+    product : shoe,<br>
+    price: 1000,<br>
+    stock: available `
+}
+function imageclick3(){
+    document.querySelector("p").innerHTML=`brand  ; samsung <br>
+    product : ac,<br>
+    price: 12000,<br>
+    stock: available `
+}
+function img(){
+    document.querySelector("p").innerHTML=" "
+    
+}
+function dbclick(){
+    document.querySelector("p").innerHTML= `do you want to by  CLICK HHERE 
+    href="www.amazon.com`;
+
+}
